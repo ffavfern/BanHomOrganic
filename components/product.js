6d9d34b6d1d0d7ecc9127d9eb703743ef73e3676
ffavfrn <ffern27744@gmail.com>
2023-03-01 08:48:28 +0700

    v.1

---
+++ components/product.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import Header from "./header";
+import Footer from "./footer";
+
+export default function product() {
+  return (
+   <>
+   <Header/>
+   <Footer/>
+   </> 
+  )
+}
+
+
+
+
---
+++ pages/footer.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+export default function footer() {
+  return (
+    <>
+      <footer className="container-fluid">
+        <div className="container">
+          <ul>
+            <li>
+              <a href="#"></a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
+  );
+}
